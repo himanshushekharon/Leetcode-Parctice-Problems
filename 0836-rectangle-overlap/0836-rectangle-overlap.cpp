0ee3d90d43f5e6a46cc1ec1b7e8627d48a0f1f5d
Himanshu Shekhar <148798610+himanshushekharon@users.noreply.github.com>
2026-09-14 21:26:22 +0530

    Time: 0 ms (100%), Space: 9.9 MB (11.03%) - LeetHub

--- 0836-rectangle-overlap/0836-rectangle-overlap.cpp
+++ 0836-rectangle-overlap/0836-rectangle-overlap.cpp
@@ -1,13 +1,28 @@
 class Solution {
 public:
     bool isRectangleOverlap(vector<int>& rec1, vector<int>& rec2) {
-     // Check if one rectangle is to the left, right, above, or below the other
-        bool is_left = rec1[2] <= rec2[0];
-        bool is_right = rec1[0] >= rec2[2];
-        bool is_bottom = rec1[3] <= rec2[1];
-        bool is_top = rec1[1] >= rec2[3];
         
-        // They overlap if they are NOT mutually exclusive of overlapping directions
-        return !(is_left || is_right || is_bottom || is_top);   
+        int x1 = rec1[0];
+        int y1 = rec1[1];
+        int x2 = rec1[2];
+        int y2 = rec1[3];
+
+        int a1 = rec2[0];
+        int b1 = rec2[1];
+        int a2 = rec2[2];
+        int b2 = rec2[3];
+
+
+        if(x2 <= a1)
+            return false;
+        if(y2 <= b1)
+            return false;
+        
+        if(x1 >= a2)
+            return false;
+        if( y1 >= b2)
+            return false;
+        
+        return true;
     }
 };
